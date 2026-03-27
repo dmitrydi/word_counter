cee923cc3b0f16b0c01ce5c0ceaf33c1c44c080b
dmitrydi <d.dmitrook@gmail.com>
2026-03-27 09:53:00 +0300

    implemented file reader

---
+++ build/CMakeFiles/word_counter.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dmitrydi/source/repos/word_counter/src/file_reader.cpp" "CMakeFiles/word_counter.dir/src/file_reader.cpp.o" "gcc" "CMakeFiles/word_counter.dir/src/file_reader.cpp.o.d"
+  "/home/dmitrydi/source/repos/word_counter/src/main.cpp" "CMakeFiles/word_counter.dir/src/main.cpp.o" "gcc" "CMakeFiles/word_counter.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
